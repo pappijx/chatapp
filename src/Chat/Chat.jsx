56ccaab2047c4c9778363b7a10cda459d075d1ab
pappijx <ayush.papani123@gmail.com>
2022-11-22 20:21:20 +0530

    latest updates on poc

---
+++ src/Chat/Chat.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef, useState } from "react";
+import io from "socket.io-client";
+const socket = io.connect("https://localhost:443");
+function Chat() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
+  const [message, setMessage] = useState("");
+  const [users, setUsers] = useState([]);
+  const [chatRoom, setChatroom] = useState("");
+  const [room, setRoom] = useState("");
+
+  const uploadFile = useRef();
+
+  useEffect(() => {
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("pong");
+    };
+  }, []);
+
+  const connectRooms = (roomName) => {
+    console.log("kyu", roomName);
+
+    socket.on("connection", (soc) => {
+      console.log("room");
+      soc.join(roomName);
+    });
+  };
+
+  useEffect(() => {
+
+  }, [chatRoom]);
+
+  useEffect(() => {
+    socket.on("recieve_message", (data) => {
+      alert(data.message);
+    });
+  }, [socket]);
+
+  const sendMessage = (message) => {
+    console.log("message to be send", message);
+    
+  };
+
+  return (
+    <div className="chat-wrapper">
+      <div className="clientslist">
+      </div>
+      <div className="chat-container">
+        <div className="inputHolder">
+          <input
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Enter messsage..."
+          />
+          <div className="chatButtonContainer">
+            <input
+              type="file"
+              ref={uploadFile}
+              onChange={(e) =>
+                sendMessage({
+                  type: "file",
+                  message: e.target.files[0],
+                })
+              }
+              hidden
+            />
+            <button onClick={() => uploadFile.current.click()}>UD</button>
+            <button
+              onClick={() =>
+                sendMessage({
+                  type: "text",
+                  message: message,
+                })
+              }
+            >
+              SD
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
